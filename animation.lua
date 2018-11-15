e7e7e45348e29ace0cb3e94bea525bc694c72dce
Italo Di Renzo <italodirenzo93@gmail.com>
2018-11-15 12:23:02 -0500

    Added basic walk animation

---
+++ animation.lua
@@ -0,0 +1,43 @@
+local Class = require 'libs/hump.class'
+
+-- Class definition
+local Animation = Class{}
+
+function Animation:init(quads, speed, frameTime)
+    self.quads = quads or {}
+    self.speed = speed or 15
+    self.frameTime = frameTime or 10
+    self.elapsed = 0
+    self.currentFrame = 1
+    self.isPlaying = false
+end
+
+function Animation:update(dt)
+    if self.isPlaying then
+        self.elapsed = self.elapsed + self.speed * dt
+        if self.elapsed >= self.frameTime then
+            self.elapsed = 0
+            -- Next frame
+            if self.currentFrame + 1 >= #self.quads then
+                self.currentFrame = 1
+            else
+                self.currentFrame = self.currentFrame + 1
+            end
+        end
+    end
+end
+
+function Animation:getCurrentFrame()
+    return self.quads[self.currentFrame]
+end
+
+function Animation:play()
+    self.isPlaying = true
+end
+
+function Animation:stop()
+    self.isPlaying = false
+    self.currentFrame = 1
+end
+
+return Animation
--- player.lua
+++ player.lua
@@ -1,6 +1,7 @@
 -- Module imports
 local Class = require 'libs/hump.class'
 local Sprite = require 'sprite'
+local Animation = require 'animation'
 
 -- Class definition
 local Player = Class{__includes = Sprite}
@@ -17,9 +18,15 @@ function Player:init(x, y)
     self.originX = self.width / 2
     self.originY = self.height / 2
 
-    self.speed = 210
-    self.animationSpeed = 15
-    self.currentFrame = love.graphics.newQuad(0, 0, self.width, self.height, texture:getWidth(), texture:getHeight())
+    self.speed = 140
+
+    local animationFrames = {
+        love.graphics.newQuad(0, 0, self.width, self.height, texture:getDimensions()),
+        love.graphics.newQuad(16 + 2, 0, self.width, self.height, texture:getDimensions()),
+        love.graphics.newQuad(32 + 2, 0, self.width, self.height, texture:getDimensions()),
+        love.graphics.newQuad(48 + 2, 0, self.width, self.height, texture:getDimensions())
+    }
+    self.animation = Animation(animationFrames, 15, 1)
 
     self.flipX = false
     self.flipY = false
@@ -29,11 +36,16 @@ function Player:update(dt)
     if love.keyboard.isDown('left') then
         self.x = self.x - self.speed * dt
         self.flipX = true
-    end
-    if love.keyboard.isDown('right') then
+        self.animation:play()
+    elseif love.keyboard.isDown('right') then
         self.x = self.x + self.speed * dt
         self.flipX = false
+        self.animation:play()
+    else
+        self.animation:stop()
     end
+
+    self.animation:update(dt)
 end
 
 -- Override
@@ -45,7 +57,7 @@ function Player:draw()
 
     love.graphics.draw(
         self.image,         -- drawable
-        self.currentFrame,  -- quad
+        self.animation:getCurrentFrame(),  -- quad
         self.x,             -- x
         self.y,             -- y
         0,                  -- rotation
